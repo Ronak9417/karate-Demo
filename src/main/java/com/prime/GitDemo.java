25d9df94338f9a6b7b9a5a7db2002758ff2a011b
rpatel942017 <rpatel942017@gmail.com>
2023-06-10 16:32:07 +0100

    Git Demo Feature

---
+++ src/main/java/com/prime/GitDemo.java
@@ -0,0 +1,11 @@
+package com.prime;
+
+/**
+ * Created By Ronak Patel
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Karate demo feature");
+    }
+}
